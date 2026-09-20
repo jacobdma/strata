450a81da979cf3c49d74d9a7d37cb25a9bc580c2
Jacob Ma <jacobdma218@gmail.com>
2026-09-19 23:12:40 -0400

    Initial Commit

---
+++ Strata/StrataApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrataApp.swift
+//  Strata
+//
+//  Created by Jacob Ma on 9/19/26.
+//
+
+import SwiftUI
+
+@main
+struct StrataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StrataTests/StrataTests.swift
@@ -0,0 +1,17 @@
+//
+//  StrataTests.swift
+//  StrataTests
+//
+//  Created by Jacob Ma on 9/19/26.
+//
+
+import Testing
+@testable import Strata
+
+struct StrataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
